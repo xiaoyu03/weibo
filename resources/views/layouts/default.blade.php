50eba8936e07b5b5785bf020d74b4689fdb4b790
xiaoyu03 <somyo97@126.com>
2020-06-22 03:30:49 +0000

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,15 +2,18 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
+
   <body>
-   @include('layouts._header')
-   <div class="container">
-    <div class="offset-md-1 col-md-10">
-      @yield('content')
-      @include('layouts._footer')
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="offset-md-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
     </div>
-   </div>
   </body>
 </html>
